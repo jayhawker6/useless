d2d1183c003e40c94628e796e618777c96bf5890
jayhawker6 <andrewgguernsey@gmail.com>
2023-03-31 15:39:07 -0500

    Consolidated function.

--- main.c
+++ main.c
@@ -5,30 +5,6 @@
 
 char buffer[BUFSIZ];
 
-void eightloop(void)
-{
-    system("clear");
-    int flag = 1;
-    while (flag)
-    {
-
-        eightball();
-
-        printf("Would you like to ask another question? (y/n): ");
-        char choice[2];
-        fgets(buffer, sizeof buffer, stdin);
-        sscanf(buffer, "%s", choice);
-        if (strcmp(choice, "n") == 0)
-        {
-            flag = 0;
-        }
-        else
-        {
-            system("clear");
-        }
-    }
-}
-
 int main(void)
 {
     int flag = 1;
@@ -47,7 +23,7 @@ int main(void)
         switch (choice)
         {
         case 1: // Magic 8-ball
-            eightloop();
+            eightball();
             break;
         case 2: // Coin flip
             coinflip();
--- main.h
+++ main.h
@@ -6,111 +6,128 @@ char buffer[BUFSIZ];
 
 void eightball(void)
 {
-    int number;
-    char result[100] = {0};
-
-    printf("\nAsk the magic 8-ball a question: ");
-    fgets(buffer, sizeof buffer, stdin);
-    fflush(stdin);
-
-    number = rand() % 30 + 1;
-
-    switch (number)
+    system("clear");
+    int flag = 1;
+    while (flag)
     {
-    case 1:
-        strcpy(result, "It is uncertain.");
-        break;
-    case 2:
-        strcpy(result, "It is not decidedly so.");
-        break;
-    case 3:
-        strcpy(result, "Without no doubt.");
-        break;
-    case 4:
-        strcpy(result, "It is not for me to say.");
-        break;
-    case 5:
-        strcpy(result, "You may or may not rely on it. It's up to you.");
-        break;
-    case 6:
-        strcpy(result, "Your answer is hazy, try again.");
-        break;
-    case 7:
-        strcpy(result, "Ask again later.");
-        break;
-    case 8:
-        strcpy(result, "Better not tell you now.");
-        break;
-    case 9:
-        strcpy(result, "Your 8 ball subscription has run out! Please pay $9.99 to your local rep to reactivate.");
-        break;
-    case 10:
-        strcpy(result, "Concentrate and ask again.");
-        break;
-    case 11:
-        strcpy(result, "I am so sorry, I can't tell you that.");
-        break;
-    case 12:
-        strcpy(result, "My sources have a lot of information, but not on that.");
-        break;
-    case 13:
-        strcpy(result, "Outlook on my abilities is not so good, sorry.");
-        break;
-    case 14:
-        strcpy(result, "Who knows?");
-        break;
-    case 15:
-        strcpy(result, "I'm not sure.");
-        break;
-    case 16:
-        strcpy(result, "I don't know.");
-        break;
-    case 17:
-        strcpy(result, "Buy a new 8 ball, this one is broken thanks to you.");
-        break;
-    case 18:
-        strcpy(result, "I'm not sure, but I'm sure you're not sure.");
-        break;
-    case 19:
-        strcpy(result, "Outlook uncertain, source responded with \"ERROR 404, Please contact administrators.\".");
-        break;
-    case 20:
-        strcpy(result, "I'm not sure, but maybe the true answer is in the friends we made along the way");
-        break;
-    case 21:
-        strcpy(result, "Can't tell you now, I'm busy.");
-        break;
-    case 22:
-        strcpy(result, "Shaking was too violent, please try again.");
-        break;
-    case 23:
-        strcpy(result, "Did not shake, please try again.");
-        break;
-    case 24:
-        strcpy(result, "Did not shake hard enough, please try again.");
-        break;
-    case 25:
-        strcpy(result, "Please check your 8 ball for any cracks or damage, then try again.");
-        break;
-    case 26:
-        strcpy(result, "Unable to read, please try again.");
-        break;
-    case 27:
-        strcpy(result, "Question not clear, please try again.");
-        break;
-    case 28:
-        strcpy(result, "Question was not sophisticated enough, please try again.");
-        break;
-    case 29:
-        strcpy(result, "ERROR: Segmentation fault predicted due to not enough memory, please download more RAM.");
-        break;
-    case 30:
-        strcpy(result, "I attempted to find what you seek, but it was too complex for a mere mortal like you.");
-        break;
-    default:
-        break;
+        int number;
+        char result[100] = {0};
+
+        printf("\nAsk the magic 8-ball a question: ");
+        fgets(buffer, sizeof buffer, stdin);
+        fflush(stdin);
+
+        number = rand() % 30 + 1;
+
+        switch (number)
+        {
+        case 1:
+            strcpy(result, "It is uncertain.");
+            break;
+        case 2:
+            strcpy(result, "It is not decidedly so.");
+            break;
+        case 3:
+            strcpy(result, "Without no doubt.");
+            break;
+        case 4:
+            strcpy(result, "It is not for me to say.");
+            break;
+        case 5:
+            strcpy(result, "You may or may not rely on it. It's up to you.");
+            break;
+        case 6:
+            strcpy(result, "Your answer is hazy, try again.");
+            break;
+        case 7:
+            strcpy(result, "Ask again later.");
+            break;
+        case 8:
+            strcpy(result, "Better not tell you now.");
+            break;
+        case 9:
+            strcpy(result, "Your 8 ball subscription has run out! Please pay $9.99 to your local rep to reactivate.");
+            break;
+        case 10:
+            strcpy(result, "Concentrate and ask again.");
+            break;
+        case 11:
+            strcpy(result, "I am so sorry, I can't tell you that.");
+            break;
+        case 12:
+            strcpy(result, "My sources have a lot of information, but not on that.");
+            break;
+        case 13:
+            strcpy(result, "Outlook on my abilities is not so good, sorry.");
+            break;
+        case 14:
+            strcpy(result, "Who knows?");
+            break;
+        case 15:
+            strcpy(result, "I'm not sure.");
+            break;
+        case 16:
+            strcpy(result, "I don't know.");
+            break;
+        case 17:
+            strcpy(result, "Buy a new 8 ball, this one is broken thanks to you.");
+            break;
+        case 18:
+            strcpy(result, "I'm not sure, but I'm sure you're not sure.");
+            break;
+        case 19:
+            strcpy(result, "Outlook uncertain, source responded with \"ERROR 404, Please contact administrators.\".");
+            break;
+        case 20:
+            strcpy(result, "I'm not sure, but maybe the true answer is in the friends we made along the way");
+            break;
+        case 21:
+            strcpy(result, "Can't tell you now, I'm busy.");
+            break;
+        case 22:
+            strcpy(result, "Shaking was too violent, please try again.");
+            break;
+        case 23:
+            strcpy(result, "Did not shake, please try again.");
+            break;
+        case 24:
+            strcpy(result, "Did not shake hard enough, please try again.");
+            break;
+        case 25:
+            strcpy(result, "Please check your 8 ball for any cracks or damage, then try again.");
+            break;
+        case 26:
+            strcpy(result, "Unable to read, please try again.");
+            break;
+        case 27:
+            strcpy(result, "Question not clear, please try again.");
+            break;
+        case 28:
+            strcpy(result, "Question was not sophisticated enough, please try again.");
+            break;
+        case 29:
+            strcpy(result, "ERROR: Segmentation fault predicted due to not enough memory, please download more RAM.");
+            break;
+        case 30:
+            strcpy(result, "I attempted to find what you seek, but it was too complex for a mere mortal like you.");
+            break;
+        default:
+            break;
+        }
+        printf("You shake the magic 8-ball and it says: %s\n", result);
+        printf("Would you like to ask another question? (y/n): ");
+        char choice[2];
+        fgets(buffer, sizeof buffer, stdin);
+        sscanf(buffer, "%s", choice);
+        if (strcmp(choice, "n") == 0)
+        {
+            flag = 0;
+        }
+        else
+        {
+            system("clear");
+        }
     }
-    printf("You shake the magic 8-ball and it says: %s\n", result);
 }
 
 void coinflip(void)
